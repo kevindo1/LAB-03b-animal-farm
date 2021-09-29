f57eee5ee58bc0c2402efebda7a0c986034893b3
Kevin Do <kevind0501@gmail.com>
2021-09-29 15:32:02 -0700

    Used javascript to add animal sound upon clicking on image

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // import functions and grab DOM elements
+const dogImage = document.getElementById('dog');
+const catImage = document.getElementById('cat');
+const horseImage = document.getElementById('horse');
 
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
+const horseSound = document.getElementById('horse-sound');
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+dogImage.addEventListener('click', ()=> {
+    dogSound.play();
+});
+
+catImage.addEventListener('click', ()=> {
+    catSound.play();
+});
+
+horseImage.addEventListener('click', ()=> {
+    horseSound.play();
+});
